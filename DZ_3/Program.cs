d58831c2f8fa9696adde56823318d1a720282b0f
Гульназ Калимуллина <gulnaz.kalimullina.2005@mail.ru>
2023-09-28 21:09:20 +0300

    Добавьте файлы проекта.

---
+++ DZ_3/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Console.WriteLine("Упражнение 4.1; преобразовать число в день и месяц");
+            try
+            {
+                Console.WriteLine("Введите число от 1 до 365:");
+                uint num = Convert.ToUInt32(Console.ReadLine());
+                DateTime Date = new DateTime().AddDays(num - 1);
+                string d = Date.ToString("M");
+                Console.WriteLine(d);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка: счёт дней начинается с 1");
+            }
+
+
+
+            Console.WriteLine("Упражнение 4.2; упр 1 с учётом исключений");
+            try
+            {
+                Console.WriteLine("Введите число от 1 до 365:");
+                uint num = Convert.ToUInt32(Console.ReadLine());
+                if (num > 365)
+                {
+                    Console.WriteLine("В году 365 дней");
+                }
+                else
+                {
+                    DateTime Date = new DateTime().AddDays(num - 1);
+                    string d = Date.ToString("M");
+                    Console.WriteLine(d);
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Ошибка: счёт дней начинается с 1");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: число не из диапазона [1;365]");
+            }*/
+
+
+
+            Console.WriteLine("Дз 4.1; предыдущие задания с учетом високосного года");
+            try
+            {
+                Console.WriteLine("Введите число от 1 до 365/366:");
+                int Num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите год:");
+                uint year = Convert.ToUInt32(Console.ReadLine());
+                double ost1 = (double)year % 4;
+                double ost2 = (double)year % 100;
+                double ost3 = (double)year % 400;
+
+                if ((ost1 == 0 && ost2 != 0) || ost3 == 0)
+                {
+                    //високосный год
+                    int[] monthsLength = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                    int sum = 0;
+                    int i = 0;
+                    while (sum < Num) sum = sum + monthsLength[i++];
+                    sum = sum - monthsLength[--i];
+                    sum = Num - sum;
+                    Console.WriteLine($"{sum}.{i + 1}.{year}");
+                }
+                else
+                {
+                    //не високосный
+                    int[] monthsLength = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+                    int sum = 0;
+                    int i = 0;
+                    while (sum < Num) sum = sum + monthsLength[i++];
+                    sum = sum - monthsLength[--i];
+                    sum = Num - sum;
+                    Console.WriteLine($"{sum}.{i + 1}.{year}");
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Год или число дня введены неверно");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Год или число дня введены неверно");
+
+            }
+        }
+    }
+}
---
+++ Userss/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Userss
+{   enum DaysOfWeek
+    {
+       Понедельник = 1,
+       Вторник,
+       Среда,
+       Четверг,
+       Пятница,
+       Суббота,
+       Воскресенье,
+    }
+    enum Cards
+    {
+        Шестёрка = 6,
+        Семёрка,
+        Восьмёрка,
+        Девятка,
+        Десятка,
+        Валет,
+        Дама,
+        Король,
+        Туз,
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Упр 2\nИгральным картам присвоены порядковые номера: «валету» — 11, «даме» — 12, «королю» — 13, «тузу» — 14.\nОпределить достоинство соответствующей карты.");
+            try
+            {
+                Console.WriteLine("Введите номер карты");
+                Cards k = (Cards)int.Parse(Console.ReadLine());
+                switch (k)
+                {
+                 case (Cards)6: Console.WriteLine((Cards)6); break;
+                 case (Cards)7: Console.WriteLine((Cards)7); break;
+                 case (Cards)8: Console.WriteLine((Cards)8); break;
+                 case (Cards)9: Console.WriteLine((Cards)9); break;
+                 case (Cards)10: Console.WriteLine((Cards)10); break;
+                 case (Cards)11: Console.WriteLine((Cards)11); break;
+                 case (Cards)12: Console.WriteLine((Cards)12); break;
+                 case (Cards)13: Console.WriteLine((Cards)13); break;
+                 case (Cards)14: Console.WriteLine((Cards)14); break;
+                 default: Console.WriteLine("Вы ввели несуществующее значение карты"); break;
+                }
+            }
+            catch (FormatException)
+            { Console.WriteLine("Вы ввели не число"); }
+            finally { Console.WriteLine("Программа запущена успешно)"); }
+
+
+            Console.WriteLine("Упр 3\nПринимает на входе строку и производит выходные данные");
+            Console.WriteLine("Введите данные в соответствии с таблицей");
+            string input = Console.ReadLine().ToLower();
+            switch (input)
+            {
+                case "jabroni": Console.WriteLine("Patron Tequila");break;
+                case "school counselor":Console.WriteLine("Anything wint Alcohol");break;
+                case "programmer": Console.WriteLine("Hipster Craft Beer"); break;
+                case "bike gang member": Console.WriteLine("Moonshine"); break;
+                case "politician": Console.WriteLine("Your tax dollars"); break;
+                case "rapper": Console.WriteLine("Cristal"); break;
+                default: Console.WriteLine("Beer"); break;
+            }
+
+
+
+            Console.WriteLine("Упр 4\nВыводит на экран название дня недели");
+            try
+            { Console.WriteLine("Введите номeр дня недели");
+              DaysOfWeek Day = (DaysOfWeek)int.Parse(Console.ReadLine());
+              switch (Day)
+              {
+                case (DaysOfWeek)1: Console.WriteLine((DaysOfWeek)1); break;
+                case (DaysOfWeek)2: Console.WriteLine((DaysOfWeek)2); break;
+                case (DaysOfWeek)3: Console.WriteLine((DaysOfWeek)3); break;
+                case (DaysOfWeek)4: Console.WriteLine((DaysOfWeek)4); break;
+                case (DaysOfWeek)5: Console.WriteLine((DaysOfWeek)5); break;
+                case (DaysOfWeek)6: Console.WriteLine((DaysOfWeek)6); break;
+                case (DaysOfWeek)7: Console.WriteLine((DaysOfWeek)7); break;
+                default: Console.WriteLine("Вы ввели несуществующий номер дня"); break;
+
+              }
+            }
+            catch (FormatException)
+            { Console.WriteLine("Вы ввели не число"); }
+        }
+    }
+}
